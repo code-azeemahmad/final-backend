222dfa9b9288500e13b9f4c235d608d0bb6c31a9
code-azeemahmad <azeemahmadd9@gmail.com>
2026-05-26 09:10:37 +0500

    feat(auth): add getCurrentUser functionality to retrieve current user

--- professional_backend/src/controllers/user.controllers.js
+++ professional_backend/src/controllers/user.controllers.js
@@ -238,7 +238,23 @@ const changeCurrentPassword = asyncHandler( async (req, res) => {
   .json(
     new ApiResponse(200, {}, "Password changed successfully!"),
   );
-
 });
 
-export { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword };
+const getCurrentUser = asyncHandler( async (req, res) => {
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(
+      200, req.user, "Current user is fetched successfullly",
+    ),
+  );
+});
+
+export { 
+  registerUser, 
+  loginUser, 
+  logoutUser, 
+  refreshAccessToken, 
+  changeCurrentPassword, 
+  getCurrentUser,
+};
