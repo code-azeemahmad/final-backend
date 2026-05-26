fba8812098cd7f9d598f3b2931f812012606d26d
code-azeemahmad <azeemahmadd9@gmail.com>
2026-05-26 09:55:52 +0500

    feat(user): add updateUserAvatar and updateUserCoverImage functionalities

--- professional_backend/src/controllers/user.controllers.js
+++ professional_backend/src/controllers/user.controllers.js
@@ -270,6 +270,53 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, user, "Account details updated successfully"));
 });
 
+const updateUserAvatar = asyncHandler(async (req, res) => {
+  const avatarLocalPath = req.file?.path;
+
+  if (!avatarLocalPath) throw new ApiError(400, "Avatar file is missing");
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+  if (!avatar?.url) throw new ApiError(400, "Error while uploading on cloudinary");
+
+  const updatedAvatar = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+      avatar: avatar.url,
+    }
+    },
+    { new: true }
+  ).select("-password");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, updatedAvatar, "Avatar updated successfully"));
+});
+
+const updateUserCoverImage = asyncHandler(async (req, res) => {
+  const coverImageLocalPath = req.file?.path;
+
+  if (!coverImageLocalPath) throw new ApiError(400, "Cover Image file is missing");
+
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!coverImage?.url) throw new ApiError(400, "Error while uploading on cloudinary");
+
+  const updatedCoverImage = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+   $set: {
+      coverImage: coverImage.url
+   }
+},
+    { new: true }
+  ).select("-password");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, updatedCoverImage, "Cover Image updated successfully"));
+});
 
 export {
   registerUser,
@@ -279,4 +326,15 @@ export {
   changeCurrentPassword,
   getCurrentUser,
   updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
 };
+
+
+/*_________________________________
+| Method | Meaning                 |
+| ------ | ----------------------- |
+| PUT    | Replace ENTIRE resource |
+| PATCH  | Update PARTIAL resource |
+|________|_________________________|
+*/
