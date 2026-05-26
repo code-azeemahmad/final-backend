0ab8313b8f977170e533a4658a0ab62b458f6bd3
code-azeemahmad <azeemahmadd9@gmail.com>
2026-05-26 09:07:24 +0500

    feat(auth): add changeCurrentPassword functionality to update user password

--- professional_backend/src/controllers/user.controllers.js
+++ professional_backend/src/controllers/user.controllers.js
@@ -219,4 +219,26 @@ const refreshAccessToken = asyncHandler(async (res, req) => {
   }
 });
 
-export { registerUser, loginUser, logoutUser, refreshAccessToken };
+const changeCurrentPassword = asyncHandler( async (req, res) => {
+  const {oldPassword, newPassword} = req.body;
+
+  // auth middleware -> req.user
+  const user = await User.findById(req.user?._id);
+  const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+
+  if (!isPasswordCorrect) {
+    throw new ApiError(400, "Invalid Old Password");
+  }
+
+  user.password = newPassword;
+  await user.save({validateBeforeSave: false});
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(200, {}, "Password changed successfully!"),
+  );
+
+});
+
+export { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword };
